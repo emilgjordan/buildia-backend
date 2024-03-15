4b3b777d6545774f7b31c92bcfb8c69239fe7089
Emil Jordan <emil.gianni.jordan@gmail.com>
2024-03-14 19:42:24 -0700

    changed to mock model factory

--- src/database/test/support/mock.model.ts
+++ src/database/test/support/mock.model.ts
@@ -1,29 +1,26 @@
-export abstract class MockModel<T> {
-  protected abstract entityStub: T;
+export function mockModelFactory<T>(entityStub: T) {
+  return class {
+    constructor(createEntityData: T) {
+      Object.assign(this, createEntityData);
+      this.constructorSpy(createEntityData);
+    }
 
-  constructor(createEntityData: T) {
-    this.constructorSpy(createEntityData);
-  }
+    constructorSpy(_createEntityData): void {}
 
-  constructorSpy(_createEntityData): void {}
+    static findOne(): { exec: () => Promise<T> } {
+      return { exec: jest.fn().mockResolvedValue(entityStub) };
+    }
 
-  findOne(): { exec: () => Promise<T> } {
-    return { exec: (): Promise<T> => Promise.resolve(this.entityStub) };
-  }
+    static find(): { exec: () => Promise<T[]> } {
+      return { exec: jest.fn().mockResolvedValue([entityStub]) };
+    }
 
-  find(): { exec: () => Promise<T[]> } {
-    return { exec: (): Promise<T[]> => Promise.resolve([this.entityStub]) };
-  }
+    save = jest.fn().mockResolvedValue(entityStub);
 
-  async save(): Promise<T> {
-    return this.entityStub;
-  }
+    static findOneAndUpdate(): { exec: () => Promise<T> } {
+      return { exec: jest.fn().mockResolvedValue(entityStub) };
+    }
 
-  findOneAndUpdate(): { exec: () => Promise<T> } {
-    return { exec: (): Promise<T> => Promise.resolve(this.entityStub) };
-  }
-
-  async deleteOne(): Promise<{ deletedCount?: number }> {
-    return { deletedCount: 1 };
-  }
+    deleteOne = jest.fn().mockResolvedValue({ deletedCount: 1 });
+  };
 }
--- src/users/test/support/user.model.ts
+++ src/users/test/support/user.model.ts
@@ -1,11 +1,6 @@
-import { MockModel } from '../../../database/test/support/mock.model';
+import { mockModelFactory } from '../../../database/test/support/mock.model';
 import { userStub } from '../stubs/user.stubs';
 import { User } from '../../schemas/user.schema';
 
-export class UserMockModel extends MockModel<User> {
-  protected entityStub = userStub();
-
-  constructor(createEntityData: User) {
-    super(createEntityData);
-  }
-}
+let userMockModel = mockModelFactory<User>(userStub());
+export { userMockModel };
